84db00b8b6c493b5912f198123844c7b61872a01
sample314github <manojgs314@gmail.com>
2024-06-05 01:59:53 -0700

    olo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let navbar = document.querySelector('.header .navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.add('active');
+    sdanc
 }
 document.querySelector('#nav-close').onclick = () =>{
     navbar.classList.remove('active');
